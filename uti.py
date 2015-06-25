2a4e3924e9b2794acb30bf72a83591c14b3b3d86
Simon Pratt <simon@pr4tt.com>
2015-06-25 13:34:39 -0400

    Now reading jpeg exif data

--- uti.py
+++ uti.py
@@ -1,3 +1,5 @@
+######################################################################
+# Parse the config
 import configparser
 
 config = configparser.ConfigParser()
@@ -6,11 +8,21 @@ config.read('uti.cfg')
 client_id = config['UTI']['client_id']
 client_secret = config['UTI']['client_secret']
 
+######################################################################
+# Parse the args
+import argparse
+
+parser = argparse.ArgumentParser(description='Upload To Imgur')
+parser.add_argument('files', metavar='FILE', nargs='+',
+                    help='files to upload')
+args = parser.parse_args()
+files = args.files
+
+######################################################################
+# Do the auth
 from imgurpython import ImgurClient
 
 client = ImgurClient(client_id, client_secret)
-
-# Authorization flow, pin example (see docs for other auth types)
 authorization_url = client.get_auth_url('pin')
 
 print('Go to the following URL:')
@@ -20,6 +32,23 @@ pin=input('And type the pin: ')
 print('pin:           {}'.format(pin))
 print('client_id:     {}'.format(client_id))
 print('client_secret: {}'.format(client_secret))
+
+# Read Exif
+import PIL.Image
+import PIL.ExifTags
+print('files:')
+for f in files:
+    print('\t{}'.format(f))
+    img = PIL.Image.open(f)
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+            for k, v in img._getexif().items()
+            if k in PIL.ExifTags.TAGS
+    }
+    for k in ['Model', 'ExposureTime', 'ISOSpeedRatings',
+              'FocalLengthIn35mmFilm', 'FNumber', 'ExifImageWidth',
+              'ExifImageHeight']:
+        print('\t\t{}: {}'.format(k, exif[k]))
 
 #credentials = client.authorize('PIN OBTAINED FROM AUTHORIZATION', 'pin')
 #client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
